6f5b8b75de3679f35d4c6c186d8a220c4b291715
REOysd <160739925+REOysd@users.noreply.github.com>
2026-02-24 13:07:08 +0900

    feat: アプリ名の変更

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Andrdroid test 2",
+                        name = "Andrdroid test 2 project",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
